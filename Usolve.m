6812f21e9dd7382dd871a9212e2cb9477c267c8b
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-04-26 10:59:23 +0200

    Create upper triangular matrix solver

---
+++ Usolve.m
@@ -0,0 +1,25 @@
+function[x, flag] = Usolve(U, b)
+    % Test dimensione
+    [n, m] = size(U);
+    flag = 0;
+    if n ~= m
+        disp('error: matrice non quadrata')
+        x = [];
+        flag = 1;
+        return
+    end
+    
+    % Test di singolarità
+    if all(diag(U)) ~= 1
+        disp('elemento diagonale nullo')
+        x = [];
+        flag = 1;
+        return
+    end
+    
+    % Inizializzazione del vettore soluzione
+    x = zeros(n, 1);
+    for i=n-1:-1:1
+        x(i) = (b(i) - U(i, i+1:n) * x(i+1:n)) / U(i, i);
+    end
+end

7743e62e81e3933ef179473f07fe34fe7e357a1f
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-05-03 10:02:23 +0200

    Fix starting point of last for loop

--- Usolve.m
+++ Usolve.m
@@ -19,7 +19,7 @@ function[x, flag] = Usolve(U, b)
     
     % Inizializzazione del vettore soluzione
     x = zeros(n, 1);
-    for i=n-1:-1:1
+    for i=n:-1:1
         x(i) = (b(i) - U(i, i+1:n) * x(i+1:n)) / U(i, i);
     end
 end
